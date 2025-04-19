b1041b9a2030b7472dfcf8f18516a3d620cc19ae
Arash Altafi <arashaltafi1377@gmail.com>
2025-04-19 08:21:35 +0330

    handle locale lang

--- app/src/main/java/ir/arash/altafi/musiccompose/MainActivity.kt
+++ app/src/main/java/ir/arash/altafi/musiccompose/MainActivity.kt
@@ -1,19 +1,36 @@
 package ir.arash.altafi.musiccompose
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import ir.arash.altafi.musiccompose.ui.navigation.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
+import ir.arash.altafi.musiccompose.utils.language.LocaleUtils
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        LocaleUtils.setLocale(this)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
         enableEdgeToEdge()
         setContent {
             AppNavigation()
         }
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(LocaleUtils.setLocale(newBase))
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        LocaleUtils.setLocale(this)
     }
 }
