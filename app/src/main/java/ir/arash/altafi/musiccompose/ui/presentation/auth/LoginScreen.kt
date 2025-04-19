509231ef356cc55091ff39d4ff117c9b3bb245c4
Arash Altafi <arashaltafi1377@gmail.com>
2025-04-19 11:26:20 +0330

    a

--- app/src/main/java/ir/arash/altafi/musiccompose/ui/presentation/auth/LoginScreen.kt
+++ app/src/main/java/ir/arash/altafi/musiccompose/ui/presentation/auth/LoginScreen.kt
@@ -1,8 +1,12 @@
 package ir.arash.altafi.musiccompose.ui.presentation.auth
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun LoginScreen(navController: NavHostController) {
+    Text(
+        "test login"
+    )
 }
--- app/src/main/java/ir/arash/altafi/musiccompose/ui/presentation/auth/RegisterScreen.kt
+++ app/src/main/java/ir/arash/altafi/musiccompose/ui/presentation/auth/RegisterScreen.kt
@@ -1,8 +1,12 @@
 package ir.arash.altafi.musiccompose.ui.presentation.auth
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun RegisterScreen(navController: NavHostController) {
+    Text(
+        text = "Register Screen"
+    )
 }
